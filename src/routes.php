ce20d605cda63d19b3764a472507944de9d82854
Freezko <freezko@gmail.com>
2014-07-10 15:31:41 +0400

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -7,8 +7,3 @@
 Route::group(Config::get('smart-assets::routing'), function() {
 	Route::get('{path}', Config::get('smart-assets::controller_action'))->where('path', '.*');
 });
-
-App::missing(function($exception)
-{
-	return Response::view('smart-assets::404', array(), 404);
-});
